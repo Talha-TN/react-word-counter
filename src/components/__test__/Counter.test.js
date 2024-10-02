406b77892935497b8bc5340b0e657cf409f1ce36
Talha-TN <nawazt964@gmail.com>
2024-10-02 16:13:56 -0700

    implemment several test cases for user input , click etc

--- src/components/__test__/Counter.test.js
+++ src/components/__test__/Counter.test.js
@@ -8,8 +8,10 @@
 // });
 /////////////////////////////////////////////////
 // test by placeholder
-import { getByTestId, render } from "@testing-library/react";
+import {  render,screen } from "@testing-library/react";
 import { Counter } from "../Counter.js";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom"
 // use descript to keep your all test cases in a single place
 describe("testing counter component", () => {
   test("testing for render the textarea", () => {
@@ -24,9 +26,45 @@ describe("testing counter component", () => {
     const charLength = getByTestId("charLength");
     expect(charLength.innerHTML).toBe("Character: 0");
   });
-  test("test for word result", () => {
+  it("test for word result", () => {
     const { getByTestId } = render(<Counter />);
     const wordLength = getByTestId("wordLength");
     expect(wordLength.innerHTML).toBe("Word: 0");
   });
+  // it is works same as test its your choice to use which one boths works same 
+  test("render the word result using screen method",()=>{
+    render(<Counter/>)
+    const wordLength= screen.getByTestId("wordLength")
+    expect(wordLength).toBeTruthy()
+  })
+  // test elements visible on screen
+  test("see the provided text is on screen or not",()=>{
+    render(<Counter/>)
+    const wordLength= screen.getByText("Word: 0")
+    expect(wordLength).toBeInTheDocument()
+  })
+  /// test case for a specific text,  if user write in text area then test wordCount and charCount 
+  test("testing text area input",()=>{
+    render(<Counter/>)
+    const textArea = screen.getByTestId("textArea")
+    const charLength = screen.getByTestId("charLength")
+    const wordLength = screen.getByTestId("wordLength")
+    // userEvent.type(textArea,"talha");
+    userEvent.type(textArea,"talha nawaz")// space is a also a character here
+    expect(charLength.innerHTML).toBe("Character: 11")
+    expect(wordLength.innerHTML).toBe("Word: 2")
+  })
+  // test for clearing textarea and test word count and char count
+  test("testing for clearing text area",()=>{
+    render(<Counter/>)
+    const textArea = screen.getByTestId("textArea")
+    const charLength = screen.getByTestId("charLength")
+    const wordLength = screen.getByTestId("wordLength")
+    const clearBtn = screen.getByTestId("clearBtn")
+    userEvent.click(clearBtn)
+    expect(charLength.innerHTML).toBe("Character: 0")
+    expect(wordLength.innerHTML).toBe("Word: 0")
+
+
+  })
 });
