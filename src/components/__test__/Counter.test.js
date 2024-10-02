cebeec52492dd761e8c892e9b63b04c95f7d8e7f
Talha-TN <nawazt964@gmail.com>
2024-10-02 13:29:00 -0700

    write test cases for Counter component

--- src/components/__test__/Counter.test.js
+++ src/components/__test__/Counter.test.js
@@ -8,11 +8,25 @@
 // });
 /////////////////////////////////////////////////
 // test by placeholder
-import { render } from "@testing-library/react";
+import { getByTestId, render } from "@testing-library/react";
 import { Counter } from "../Counter.js";
-test("testing for render the textarea", () => {
-  const { getByPlaceholderText } = render(<Counter />);
-  const textArea = getByPlaceholderText("Type or paste your text");
-  expect(textArea).toBeTruthy();
+// use descript to keep your all test cases in a single place
+describe("testing counter component", () => {
+  test("testing for render the textarea", () => {
+    const { getByPlaceholderText } = render(<Counter />);
+    const textArea = getByPlaceholderText("Type or paste your text");
+    expect(textArea).toBeTruthy();
+  });
+  // note here the test case is case sensitive mean it will also checks if the alphabet is capital or small
+
+  test("test for character  result", () => {
+    const { getByTestId } = render(<Counter />);
+    const charLength = getByTestId("charLength");
+    expect(charLength.innerHTML).toBe("Character: 0");
+  });
+  test("test for word result", () => {
+    const { getByTestId } = render(<Counter />);
+    const wordLength = getByTestId("wordLength");
+    expect(wordLength.innerHTML).toBe("Word: 0");
+  });
 });
-// note here the test case is case sensitive mean it will also checks if the alphabet is capital or small
